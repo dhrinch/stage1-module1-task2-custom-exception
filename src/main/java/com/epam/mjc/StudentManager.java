d6c6692a2921be658343e840fe7f358a00c70d68
dhrinch <dhrinch@gmail.com>
2022-09-14 01:10:20 +0200

    Implemented solution

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,8 +5,12 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws CustomException {
+    try {
+      return Student.getValueOf(studentID);
+    } catch (IllegalArgumentException e) {
+      throw new CustomException("Could not find student with ID " + studentID);
+    }
   }
 
   public static void main(String[] args) {
