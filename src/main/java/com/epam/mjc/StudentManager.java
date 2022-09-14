e4f44e37eaa3e5a87cca83265e800a9e65e42729
dhrinch <dhrinch@gmail.com>
2022-09-14 15:14:43 +0200

    Fixed

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -7,7 +7,7 @@ public class StudentManager {
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) throws CustomException {
-    if(Arrays.asList(IDs).contains(studentID) && Student.getValueOf(studentID)!=null) {
+    if(Student.getValueOf(studentID)!=null) {
       return Student.getValueOf(studentID);
     } else {
       throw new CustomException("Could not find student with ID " + studentID);
